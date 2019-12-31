581d20d8e359e10b7e622f4e26cabe00b73dbb10
welvim <wahyuajisulaiman@gmail.com>
2019-12-31 08:58:35 +0700

    added view errors

--- assets/js/script.js
+++ assets/js/script.js
@@ -7,8 +7,3 @@ $( '.friend-drawer--onhover' ).on( 'click',  function() {
 $('.cached').on('click', function() {
 	$('.friend-drawer--onhover').hide('slow').show('slow');
 });
-
-$('.message').on('click', function() {
-	$('.messaging').hide('slow');
-	$('.friend-list').show('slow');
-})
